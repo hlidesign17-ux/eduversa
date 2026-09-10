eb0b3a367ebe241026aeb943d5e14681ab86bcd9
hlidesign17-ux <hlidesign17@gmail.com>
2026-09-10 09:05:33 +0700

    last

--- js/soal-data.js
+++ js/soal-data.js
@@ -2,75 +2,164 @@ export const DATA_SOAL = [
   {
     id: 1,
     image: "assets/images/soal01.png",
-    question: "Berdasarkan gambar di atas, apakah nama komponen tersebut?",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
+    question:
+      "Apa yang terjadi saat dua tombol di atas ditekan secara bersamaan?",
+    options: [
+      "Membuka File Explorer",
+      "Membuka Task Manager",
+      "Membuka Settings",
+      "Membuka Command Prompt",
+    ],
     correctAnswer: 0, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 2,
     image: "assets/images/soal02.png",
-    question:
-      "Perhatikan gambar di atas. Fungsi utama dari bagian tersebut adalah...",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
-    correctAnswer: 1,
+    question: "Perhatikan ikon di atas. Ikon tersebut menunjukkan ...",
+    options: ["folder", "file", "Menu Bar", "Tab Menu"],
+    correctAnswer: 0,
   },
   {
     id: 3,
     image: "assets/images/soal03.png",
-    question: "Pernyataan yang tepat sesuai dengan ilustrasi gambar adalah...",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
-    correctAnswer: 2,
+    question:
+      "Gambar yang berfungsi untuk membuat nama pada file scratch ditunjukkan oleh gambar dengan nomor berapa...?",
+    options: ["Gambar No 1", "Gambar No 2", "Gambar No 3", "Gambar No 4"],
+    correctAnswer: 0,
   },
   {
     id: 4,
     image: "assets/images/soal04.png",
-    question: "Berdasarkan gambar, hasil dari proses tersebut adalah...",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
-    correctAnswer: 3,
+    question:
+      "Gambar-gambar di atar merupakan bagian scratch yang terdapat pada bagian...",
+    options: ["Tab Menu", "Menu Bar", "Code", "Properties"],
+    correctAnswer: 1,
   },
   {
     id: 5,
     image: "assets/images/soal05.png",
     question:
-      "Langkah selanjutnya yang harus dilakukan berdasarkan gambar di atas adalah...",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
-    correctAnswer: 0,
+      "Nama Tab yang kita gunakan untuk mengubah bentuk dan mengubah warna sprite secara manual adalah...",
+    options: ["Edit", "Backdrop", "Costumes", "Code"],
+    correctAnswer: 2,
   },
   {
     id: 6,
     image: "assets/images/soal06.png",
     question:
-      "Perhatikan pola pada gambar di atas. Mana opsi yang paling sesuai?",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
-    correctAnswer: 1,
+      "Perhatikan pola pada gambar di atas. Berapa garis yang dapat dibentuk oleh gambar di atas?",
+    options: ["4 garis", "5 garis", "6 garis", "7 garis"],
+    correctAnswer: 2,
   },
   {
     id: 7,
     image: "assets/images/soal07.png",
-    question: "Simbol pada gambar di atas mengindikasikan bahwa...",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
+    question: "Apa nama kelompok kode yang ditunjukkan oleh gambar di atas?",
+    options: ["Event", "Control", "Motion", "Looks"],
     correctAnswer: 2,
   },
   {
     id: 8,
     image: "assets/images/soal08.png",
     question:
-      "Berdasarkan diagram pada gambar, hubungan antara kedua variabel adalah...",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
-    correctAnswer: 0,
+      "Perhatikan gambar di atas dengan teliti. Bagaimana kalimat kode yang tepat agar sprite Mario dapat berada tepat di bawah blok tanda tanya?",
+    options: [
+      "change y by 6",
+      "change x by 6",
+      "change y by -6",
+      "change x by -6",
+    ],
+    correctAnswer: 1,
   },
   {
     id: 9,
     image: "assets/images/soal09.png",
-    question: "Analisis gambar di atas dan tentukan mana opsi yang benar!",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
+    question:
+      "Perhatikan gambar di atas dengan sangat teliti! Bagaimana kalimat kode yang tepat agar sprite Mario dapat berada tepat di di bawah blok tanda tanya?",
+    options: [
+      "change x by -3",
+      "change x by -4",
+      "change x by -5",
+      "change x by -6",
+    ],
     correctAnswer: 3,
   },
   {
     id: 10,
     image: "assets/images/soal10.png",
-    question: "Kesimpulan dari materi pada gambar di atas adalah...",
-    options: ["Pilihan A", "Pilihan B", "Pilihan C", "Pilihan D"],
+    question:
+      "Perhatikan gambar di atas dengan sangat teliti! Bagaimana kalimat kode yang tepat agar kaki dari sprite Mario dapat berada sama tinggi dengan pipa yang ada di sebelahnya?",
+    options: [
+      "change y by 3",
+      "change x by 3",
+      "change y by 2",
+      "change y by -3",
+    ],
+    correctAnswer: 0,
+  },
+  {
+    id: 11,
+    image: "assets/images/soal11.png",
+    question:
+      "Perhatikan gambar di atas dengan sangat teliti! Bagaimana kalimat kode yang tepat agar sprite Mario dapat mencapai jamur?",
+    options: [
+      "change y by -2, change x by 2",
+      "change x by 2, change y by -2",
+      "change x by 2, change x by -2",
+      "change y by -2, change y by -2",
+    ],
     correctAnswer: 1,
+  },
+  {
+    id: 12,
+    image: "assets/images/soal12.png",
+    question:
+      "apa kalimat kode yang tepat agar mario dapat menghadap ke arah tiang bendera dengan tepat?",
+    options: [
+      "change x by 4",
+      "point in direction 90",
+      "change y by 4",
+      "point in direction -90",
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 13,
+    image: "assets/images/soal13.png",
+    question:
+      "apa kalimat kode yang sangat tepat agar mario dapat meraih jamur?",
+    options: [
+      "point in direction 90, change x by 2",
+      "change x by 2, point in direction 90",
+      "change x by 2",
+      "tidak ada yang benar",
+    ],
+    correctAnswer: 0,
+  },
+  {
+    id: 14,
+    image: "assets/images/soal14.png",
+    question:
+      "apa kalimat kode yang sangat tepat agar mario dapat meraih jamur?",
+    options: [
+      "point in direction 90, change x by 5",
+      "point in direction 90, change x by -5",
+      "point in direction -90, change x by 5",
+      "point in direction -90, change x by -5",
+    ],
+    correctAnswer: 0,
+  },
+  {
+    id: 15,
+    image: "assets/images/soal15.png",
+    question:
+      "apa kalimat kode yang sangat tepat agar mario dapat meraih kotak tanda tanya dengan selamat?",
+    options: [
+      "point in direction 90,<br>change x by 1,<br> change y by -2,<br> point in direction -90,<br> change x by -1",
+      "point in direction 90,<br>change x by 1,<br> change y by -2,<br> change x by -1",
+      "change x by -2, <br> change y by -2,<br>point in direction 90, <br>change x by 2",
+      "change x by -2, <br> change y by -2,<br>change x by 2",
+    ],
+    correctAnswer: 0,
   },
 ];
