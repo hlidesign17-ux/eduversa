fe6956bbf9cb34ac160a3b01558dd0106ee9fe4a
hlidesign17-ux <hlidesign17@gmail.com>
2026-09-10 16:08:34 +0700

    alhamdulillah

--- js/soal-data.js
+++ js/soal-data.js
@@ -17,7 +17,8 @@ export const DATA_SOAL = [
     image: "assets/images/soal02.png",
     question: "Perhatikan ikon di atas. Ikon tersebut menunjukkan ...",
     options: ["folder", "file", "Menu Bar", "Tab Menu"],
-    correctAnswer: 0,
+
+    correctAnswer: 0, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 3,
@@ -25,15 +26,15 @@ export const DATA_SOAL = [
     question:
       "Gambar yang berfungsi untuk membuat nama pada file scratch ditunjukkan oleh gambar dengan nomor berapa...?",
     options: ["Gambar No 1", "Gambar No 2", "Gambar No 3", "Gambar No 4"],
-    correctAnswer: 0,
+    correctAnswer: 0, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 4,
     image: "assets/images/soal04.png",
     question:
-      "Gambar-gambar di atar merupakan bagian scratch yang terdapat pada bagian...",
+      "Gambar-gambar di atas merupakan bagian scratch yang terdapat pada bagian...",
     options: ["Tab Menu", "Menu Bar", "Code", "Properties"],
-    correctAnswer: 1,
+    correctAnswer: 1, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 5,
@@ -41,7 +42,7 @@ export const DATA_SOAL = [
     question:
       "Nama Tab yang kita gunakan untuk mengubah bentuk dan mengubah warna sprite secara manual adalah...",
     options: ["Edit", "Backdrop", "Costumes", "Code"],
-    correctAnswer: 2,
+    correctAnswer: 2, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 6,
@@ -49,14 +50,14 @@ export const DATA_SOAL = [
     question:
       "Perhatikan pola pada gambar di atas. Berapa garis yang dapat dibentuk oleh gambar di atas?",
     options: ["4 garis", "5 garis", "6 garis", "7 garis"],
-    correctAnswer: 2,
+    correctAnswer: 2, // Index jawaban benar (0=A, 1=B, 2=C, 3=D) 6 garis
   },
   {
     id: 7,
     image: "assets/images/soal07.png",
     question: "Apa nama kelompok kode yang ditunjukkan oleh gambar di atas?",
     options: ["Event", "Control", "Motion", "Looks"],
-    correctAnswer: 2,
+    correctAnswer: 2, // Index jawaban benar (0=A, 1=B, 2=C, 3=D) motion
   },
   {
     id: 8,
@@ -69,20 +70,20 @@ export const DATA_SOAL = [
       "change y by -6",
       "change x by -6",
     ],
-    correctAnswer: 1,
+    correctAnswer: 1, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 9,
     image: "assets/images/soal09.png",
     question:
-      "Perhatikan gambar di atas dengan sangat teliti! Bagaimana kalimat kode yang tepat agar sprite Mario dapat berada tepat di di bawah blok tanda tanya?",
+      "Perhatikan gambar di atas dengan sangat teliti! Bagaimana kalimat kode yang tepat agar sprite Mario dapat berada tepat di bawah blok tanda tanya?",
     options: [
       "change x by -3",
       "change x by -4",
       "change x by -5",
       "change x by -6",
     ],
-    correctAnswer: 3,
+    correctAnswer: 3, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 10,
@@ -95,7 +96,7 @@ export const DATA_SOAL = [
       "change y by 2",
       "change y by -3",
     ],
-    correctAnswer: 0,
+    correctAnswer: 0, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 11,
@@ -108,52 +109,52 @@ export const DATA_SOAL = [
       "change x by 2, change x by -2",
       "change y by -2, change y by -2",
     ],
-    correctAnswer: 1,
+    correctAnswer: 1, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 12,
     image: "assets/images/soal12.png",
     question:
-      "apa kalimat kode yang tepat agar mario dapat menghadap ke arah tiang bendera dengan tepat?",
+      "Apa kalimat kode yang tepat agar mario dapat menghadap ke arah tiang bendera dengan tepat?",
     options: [
       "change x by 4",
       "point in direction 90",
       "change y by 4",
       "point in direction -90",
     ],
-    correctAnswer: 1,
+    correctAnswer: 1, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 13,
     image: "assets/images/soal13.png",
     question:
-      "apa kalimat kode yang sangat tepat agar mario dapat meraih jamur?",
+      "Apa kalimat kode yang sangat tepat agar mario dapat meraih jamur?",
     options: [
       "point in direction 90, change x by 2",
       "change x by 2, point in direction 90",
       "change x by 2",
       "tidak ada yang benar",
     ],
-    correctAnswer: 0,
+    correctAnswer: 0, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 14,
     image: "assets/images/soal14.png",
     question:
-      "apa kalimat kode yang sangat tepat agar mario dapat meraih jamur?",
+      "Apa kalimat kode yang sangat tepat agar mario dapat meraih jamur?",
     options: [
       "point in direction 90, change x by 5",
       "point in direction 90, change x by -5",
       "point in direction -90, change x by 5",
       "point in direction -90, change x by -5",
     ],
-    correctAnswer: 0,
+    correctAnswer: 3, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
   },
   {
     id: 15,
     image: "assets/images/soal15.png",
     question:
-      "apa kalimat kode yang sangat tepat agar mario dapat meraih kotak tanda tanya dengan selamat?",
+      "Apa kalimat kode yang sangat tepat agar mario dapat meraih kotak tanda tanya dengan selamat?",
     options: [
       "point in direction 90,<br>change x by 1,<br> change y by -2,<br> point in direction -90,<br> change x by -1",
       "point in direction 90,<br>change x by 1,<br> change y by -2,<br> change x by -1",
@@ -161,5 +162,5 @@ export const DATA_SOAL = [
       "change x by -2, <br> change y by -2,<br>change x by 2",
     ],
     correctAnswer: 0,
-  },
+  }, // Index jawaban benar (0=A, 1=B, 2=C, 3=D)
 ];
